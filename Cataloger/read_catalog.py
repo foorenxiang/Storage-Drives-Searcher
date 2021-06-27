6dca0a277ea9560edc7b72d51425660a50408b2d
Ren Xiang <foorenxiang@gmail.com>
2021-06-27 19:35:23 +0800

    added more stats and options

--- Cataloger/read_catalog.py
+++ Cataloger/read_catalog.py
@@ -6,8 +6,31 @@ catalog_file = from_root("catalog.json")
 drive = "WD_BLACK"
 
 with open(catalog_file, "r") as fp:
-    drive_descriptor = json.load(fp)
+    drive_descriptors = json.load(fp)
 
-ic(drive_descriptor[drive])
 
-ic(drive_descriptor.keys())
+drives_catalogued = drive_descriptors.keys()
+
+enumerated_drives = tuple(enumerate(drives_catalogued))
+
+items_catalogued = 0
+for descriptor in drive_descriptors.values():
+    items_catalogued += len(descriptor["paths"])
+
+
+items_catalogued = 0
+for descriptor in drive_descriptors.values():
+    items_catalogued += len(descriptor["paths"])
+print("\nFiles catalogued:", items_catalogued)
+print("\nDrives catalogued:")
+# [print(drive) for drive in drives_catalogued]
+
+[print(f"{drive_name}:", enum) for enum, drive_name in enumerated_drives]
+selected_drive_index = input("\nSelect drive index to show manifest for: ")
+
+try:
+    print("Loading...")
+    selected_drive_name = enumerated_drives[int(selected_drive_index)][1]
+    ic(drive_descriptors[selected_drive_name])
+except IndexError:
+    print("Invalid selection!")
