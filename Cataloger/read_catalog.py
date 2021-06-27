3df23cb4294bdbed56dbc70b1a9a467e91c6f768
Ren Xiang <foorenxiang@gmail.com>
2021-06-28 00:17:56 +0800

    added enumeration refactoring

--- Cataloger/read_catalog.py
+++ Cataloger/read_catalog.py
@@ -3,7 +3,6 @@ import json
 from icecream import ic
 
 catalog_file = from_root("catalog.json")
-drive = "WD_BLACK"
 
 with open(catalog_file, "r") as fp:
     drive_descriptors = json.load(fp)
@@ -11,26 +10,24 @@ with open(catalog_file, "r") as fp:
 
 drives_catalogued = drive_descriptors.keys()
 
-enumerated_drives = tuple(enumerate(drives_catalogued))
-
-items_catalogued = 0
-for descriptor in drive_descriptors.values():
-    items_catalogued += len(descriptor["paths"])
-
+enumeration_offset = 1
+enumerated_drives = tuple(enumerate(drives_catalogued, enumeration_offset))
 
 items_catalogued = 0
 for descriptor in drive_descriptors.values():
     items_catalogued += len(descriptor["paths"])
 print("\nFiles catalogued:", items_catalogued)
 print("\nDrives catalogued:")
-# [print(drive) for drive in drives_catalogued]
 
 [print(f"{drive_name}:", enum) for enum, drive_name in enumerated_drives]
 selected_drive_index = input("\nSelect drive index to show manifest for: ")
 
 try:
     print("Loading...")
-    selected_drive_name = enumerated_drives[int(selected_drive_index)][1]
+    drive_name_index = 1
+    selected_drive_name = enumerated_drives[
+        int(selected_drive_index) - enumeration_offset
+    ][drive_name_index]
     ic(drive_descriptors[selected_drive_name])
 except IndexError:
     print("Invalid selection!")
